747aca83927971b2a1c2935de06ab0c0a320cee3
Hirusha2000 <2000hirushaudara@gmail.com>
2024-02-17 16:53:04 +0530

    new updates

--- src/main/java/com/geeganage/learnspring/HelloWorldConfiguration.java
+++ src/main/java/com/geeganage/learnspring/HelloWorldConfiguration.java
@@ -4,7 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 
-record Person (String name, int age) {};
+record Person (String name, int age ,Address address) {};
 
 record Address(String firstLine, String city) {};
 @Configuration
@@ -24,15 +24,37 @@ public class HelloWorldConfiguration {
 	
 	@Bean
 	public Person person () {
-		return new Person("udara",26);
+		return new Person("udara",26, new Address("Pragathi mawatha","Galle"));
+		
+		
+	}
+	
+	@Bean
+	public Person person2MethodCall () {
+		return new Person(name(),age(),address());
 		
 		
 	}
 	
 	
 	@Bean
+	public Person person3Parameters (String name , int age , Address address3) {
+		return new Person(name,age,address3);
+		
+		
+	}
+	
+	@Bean(name="address2")
 	public Address address () {
 		return new Address("Meegoda","Galle");
+		
+		
+	}
+	
+	
+	@Bean(name="address3")
+	public Address address3 () {
+		return new Address("Pilana","Galle");
 		
 		
 	}
